62ac92b6b8b666f011d4949c6e09ef3229ecc782
Thien Phuc <thienphuc484@gmail.com>
2024-01-01 13:40:53 +0700

    Add Vuex.

---
+++ src/store/index.js
@@ -0,0 +1,29 @@
+
+import { createStore } from 'vuex';
+
+const store = createStore({
+  state: {
+    token: null,
+  },
+  mutations: {
+    setToken(state, token) {
+      state.token = token;
+    },
+    clearToken(state) {
+      state.token = null;
+    },
+  },
+  actions: {
+    setToken({ commit }, token) {
+      commit('setToken', token);
+    },
+    clearToken({ commit }) {
+      commit('clearToken');
+    },
+  },
+  getters: {
+    getToken: (state) => state.token,
+  },
+});
+
+export default store;
